f033dac4bdb8c2312d57b4edc54290e172c75b96
Axl-Lvy <axeldecesson@live.fr>
2023-01-06 14:32:34 +0100

    users creation

--- TODO.md
+++ TODO.md
@@ -15,7 +15,7 @@
     - [X] UML diagram
     - [X] Migrations
     - [X] Import data
-    - [ ] Authentification
+    - [X] Authentification
 
 ## Optional features
 
@@ -23,5 +23,6 @@
 
 - [ ] Use `update_conflicts = True` in bulk_create. Only works with Django > 4.1
 - [X] Compress the sequences : 10 Mo can be free if we use the compression in [utils.py](source/bacterial_genome_annotation/utils.py)
+- [X] Default group Reader
 - [ ] Make a command to create a bunch of example users and new annotations
 
--- source/bacterial_genome_annotation/management/commands/create-users.py
+++ source/bacterial_genome_annotation/management/commands/create-users.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand
 from ...models import User
 from django.contrib.auth.models import Group
-
+import os.path
 class Command(BaseCommand):
     def add_arguments(self, parser):
         # Positional arguments
@@ -21,4 +21,28 @@ class Command(BaseCommand):
         admin.groups.add(annotators)
         admin.groups.add(validators)
         admin.groups.add(admins)
+        
+        with open(os.path.dirname(__file__) + '/../../assets/default_users.txt') as file:
+            lines = file.readlines()
+            while lines[0].startswith('#'):
+                lines = lines[1:]
+            lines = lines[1:]
+            while len(lines)!=0:
+                email = lines[0][:-1]
+                password = lines[1][:-1]
+                group = lines[2][:-1]
+                User.objects.create_user(email, password)
+                print(email)
+                print('a')
+                u = User.objects.get(email=email)
+                if group=='Annotators':
+                    u.groups.add(annotators)
+                elif group=='Validators':
+                    u.groups.add(annotators)
+                    u.groups.add(validators)
+                elif group=='Admins':
+                    u.groups.add(annotators)
+                    u.groups.add(validators)
+                    u.groups.add(admins)
+                lines = lines[4:]
         
