d1952ae9d3e175b7cda23cfbb75270e5927e6739
djamelboubred <95863768+djamelboubred@users.noreply.github.com>
2023-01-08 17:25:49 +0100

    Add files via upload

---
+++ projet_web/source/source/urls.py
@@ -0,0 +1,33 @@
+"""source URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from bacterial_genome_annotation import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.Base),
+    path('home.html/',views.home),
+    path('annoter.html/',views.annoter),
+    path('AddGenome.html/',views.AddGenome),
+    path('AddGenome.html/',views.Account),
+    path('AddGenome.html/',views.LoginPage), 
+]
+
+urlpatterns += static(settings.STATIC_URL,
+ document_root=settings.STATIC_ROOT)
