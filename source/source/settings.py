70f966b6f2a31e567dff5e9c20b6b4d1c0fcf20a
Axl-Lvy <axeldecesson@live.fr>
2023-02-02 16:24:45 +0100

    prepare for vercel

--- source/source/settings.py
+++ source/source/settings.py
@@ -81,10 +81,10 @@ WSGI_APPLICATION = 'source.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
@@ -126,7 +126,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'root')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'boot'),
-    os.path.join(BASE_DIR, 'static'),
+    BASE_DIR/'static',
 ]
 STATIC_ROOT = BASE_DIR/'staticfiles'
 
