f29ff7176ea45f57265db7146593aec7f6c67185
Louis <louis.perrin@ens-rennes.fr>
2023-02-10 19:41:00 +0100

    require fasta files when adding genome

--- source/bacterial_genome_annotation/forms.py
+++ source/bacterial_genome_annotation/forms.py
@@ -5,6 +5,7 @@ This is the forms file. It contains all forms to prompt data to the user.
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm, ReadOnlyPasswordHashField
 from django import forms
 from .models import User
+from django.core.exceptions import ValidationError
 
 
 class UserCreationForm(UserCreationForm):
@@ -32,7 +33,7 @@ class UserSearchForm(forms.Form):
     show_only_favorites = forms.BooleanField(required=False)
 
 
-class SearchTypeForm(forms.Form):
+class SearchForm(forms.Form):
     MY_CHOICES = [("cds", "nucleic"), ("pep", "peptidic"), ("gen", "genome")]
     type = forms.ChoiceField(choices=MY_CHOICES, widget=forms.Select(attrs={'onchange': 'submit();'}), required=False)
 
@@ -88,10 +89,22 @@ class ContactForm(forms.Form):
     email = forms.EmailField()
     content = forms.CharField(widget=forms.Textarea)
 
+def validate_file_extension(value):
+    # Allowed file types
+    ALLOWED_EXTENSIONS = ['fa','fasta']
+
+    # Get the file extension from the file name
+    ext = value.name.split('.')[-1]
+    # If the extension is not in the allowed list, raise a validation error
+    if ext not in ALLOWED_EXTENSIONS:
+        raise ValidationError(f'File type {ext} is not allowed. Allowed types: {", ".join(ALLOWED_EXTENSIONS)}')
+
 class AddGenomeForm(forms.Form):
     ID = forms.CharField(max_length=100,required=True)
-    fasta_file = forms.FileField(required=True)
-    cds_file = forms.FileField(required=True)
-    pep_file = forms.FileField(required=True)
+    fasta_file = forms.FileField(required=True,validators=[validate_file_extension])
+    cds_file = forms.FileField(required=True,validators=[validate_file_extension])
+    pep_file = forms.FileField(required=True,validators=[validate_file_extension])
+
+
 
 # default=''
